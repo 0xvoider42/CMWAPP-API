b1c235c1bc8109af5d8ced80dc6e50a56161cbe5
0xvoider42 <shautidzenikoloz@gmail.com>
2024-12-23 16:51:33 +0200

    adds delete

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   app.enableCors();
 
   // Global prefix
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api-v1');
 
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');

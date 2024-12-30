f37814b1cb3ee3e3521680e886de307784aaa1ee
0xvoider42 <shautidzenikoloz@gmail.com>
2024-12-30 11:44:27 +0200

    deploy refactor

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,16 @@ import pino from 'pino';
 async function bootstrap() {
   const logger = pino({
     level: process.env.LOG_LEVEL || 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-      },
-    },
+    ...(process.env.NODE_ENV === 'development'
+      ? {
+          transport: {
+            target: 'pino-pretty',
+            options: {
+              colorize: true,
+            },
+          },
+        }
+      : {}),
   });
 
   const app = await NestFactory.create(AppModule);

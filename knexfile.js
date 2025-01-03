03d1d9014e0357680f08bf93f36a28df57b7f9fb
0xvoider42 <shautidzenikoloz@gmail.com>
2025-01-03 17:14:55 +0200

    edit knexfile

--- knexfile.js
+++ knexfile.js
@@ -19,4 +19,18 @@ module.exports = {
       directory: 'migrations',
     },
   },
+  production: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      tableName: 'migrations',
+      directory: 'migrations',
+    },
+  },
 };

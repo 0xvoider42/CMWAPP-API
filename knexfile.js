1674f3531c61d980aba9a719558404665c11a9f9
0xvoider42 <shautidzenikoloz@gmail.com>
2025-01-03 17:09:24 +0200

    refactors migrations

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,22 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-const path = require('path');
-const configuration = require('./dist/config/configuration.js').default();
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('dotenv').config();
 
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
-  client: configuration.database.client,
-  connection: configuration.database.connection,
-  pool: configuration.database.pool,
-  migrations: {
-    directory: path.join(__dirname, 'src/database/migrations'),
-    tableName: 'knex_migrations',
-  },
-  seeds: {
-    directory: path.join(__dirname, 'src/database/seeds'),
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      tableName: 'migrations',
+      directory: 'migrations',
+    },
   },
 };
---
+++ migrations/20241220132156_create_payouts_table.js
@@ -0,0 +1,35 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  const exists = await knex.schema.hasTable('payouts');
+
+  if (!exists) {
+    await knex.schema.createTable('payouts', (table) => {
+      table.increments('id').primary();
+      table
+        .integer('campaign_id')
+        .unsigned()
+        .references('id')
+        .inTable('campaigns')
+        .onDelete('CASCADE');
+      table.string('country', 2).notNullable();
+      table.decimal('amount', 10, 2).notNullable();
+      table.string('currency', 3).defaultTo('EUR');
+      table.boolean('is_active').defaultTo(true);
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+
+      table.unique(['campaign_id', 'country']);
+    });
+  }
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+  await knex.schema.dropTable('payouts');
+};
